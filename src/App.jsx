3237dcd2ef2a6e4237dbb959d1849d57ef3cb646
vahid4 <vahid.gasanin1gmail.com>
2022-08-15 16:34:32 +0200

    nesto

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import About from "./components/About";
-
+//vahid je nesto uradio
 function App() {
   return <div>
     <Nav/>
