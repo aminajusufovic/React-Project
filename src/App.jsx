d984cf5eec1cce6bbbb9c75c694c02ad8a829dcc
Filip <filip.ratkovic.fr@gmail.com>
2022-08-15 12:00:53 +0200

    Dodavanje home i about

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import { Routes, Route, Link } from 'react-router-dom';
 import Nav from "./components/Nav";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return <div>
     <Nav/>
 
     <Routes>
-      <Route path={"/"} element={<div>Home</div>} />
-      <Route path={"/about"} element={<div>About</div>} />
+      <Route path={"/"} element={<Home/>} />
+      <Route path={"/about"} element={<About/>} />
       <Route path={"/products"} element={<div>Products</div>} />
     </Routes>
   </div>;
