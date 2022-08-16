85301e75f16c041c8c06e103485cfd6b09e0dbb0
Amina <aminajusufovic4@gmail.com>
2022-08-16 20:45:57 +0200

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from "react";
+import "../App.css";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <main>
+      <img className="aboutSlika" src="#" alt="nice desk" />
+
+      <div className="aboutTitle">
+        <h2>Our story</h2>
+      </div>
+      <p className="aboutTekst">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+        accusantium sapiente tempora sed dolore esse deserunt eaque excepturi,
+        delectus error accusamus vel eligendi, omnis beatae. Quisquam, dicta.
+        Eos quod quisquam esse recusandae vitae neque dolore, obcaecati incidunt
+        sequi blanditiis est exercitationem molestiae delectus saepe odio
+        eligendi modi porro eaque in libero minus unde sapiente consectetur
+        architecto. Ullam rerum, nemo iste ex, eaque perspiciatis nisi, eum
+        totam velit saepe sed quos similique amet. Ex, voluptate accusamus
+        nesciunt totam vitae esse iste.
+      </p>
+    </main>
+  );
 }
+
 export default About;
