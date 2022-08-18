799aa3ff5e568fbfae7881d2203d6262de415f63
Filip <filip.ratkovic.fr@gmail.com>
2022-08-18 23:51:02 +0200

    single product css

--- src/components/ProductPage/SingleProduct.jsx
+++ src/components/ProductPage/SingleProduct.jsx
@@ -3,18 +3,15 @@ import { Link } from 'react-router-dom';
 
 function SingleProduct(singleProduct) {
     const { id, category, company, description, image, name, price, shipping, colors } = singleProduct.singleProduct;
-    console.log(singleProduct.singleProduct);
-    //     category: "office"
-    // colors: (3) ['#ff0000', '#00ff00', '#0000ff']
-    // company: "marcos"
-    // description: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge"
-    // id: "recZkNf2kwmdBcqd0"
-    // image: "https://dl.airtable.com/.attachmentThumbnails/e8bc3791196535af65f40e36993b9e1f/438bd160?ts=1660684024&userId=usrQMwWEPx18KgLcP&cs=72c50e36cc2728bc"
-    // name: "accent chair"
-    // price: 25999
-    // shipping: true
+    console.log(singleProduct.singleProduct)
     return (
         <main className="single-product-container">
+            <div className="single-products-nav">
+                <Link className="single-products-link" to="/"><h3 id="link-color">Home</h3></Link>
+                <Link className="single-products-link" to="/products"><h3 id="link-color">/  Products</h3></Link>
+                <h3>/ {name}</h3>
+            </div>
+
             <div className="single-product-back">
                 <Link to="/products"><button>BACK TO PRODUCTS</button></Link>
             </div>
@@ -25,20 +22,32 @@ function SingleProduct(singleProduct) {
                 </div>
 
                 <div className="single-product-text">
-                    <h1>{name}</h1>
+                    <h1>{name.toUpperCase()}</h1>
                     <div className="single-product-reviews">******</div>
-                    <h4>{price}</h4>
+                    <h4 id="single-price">${price / 100}</h4>
                     <br />
-                    <p>{description}</p>
-                    <div className="single-product-available">
-                        Available :  nestoo
+                    <p id="single-description">{description}</p>
+                    <div className="single-left">
+                        <div className="single-text-left">
+                            <h4>Available:</h4>
+                        </div>
+                        <p>In Stock</p>
                     </div>
-                    <div className="single-product-id">
-                        SKU:     {id}
+
+                    <div className="single-left">
+                        <div className="single-text-left">
+                            <h4>SKU:</h4> 
+                        </div>
+                        <p>{id}</p>
                     </div>
-                    <div className="single-product-brand">
-                        Brand: {company}
+
+                    <div className="single-left">
+                        <div className="single-text-left">
+                            <h4>Brand:</h4>
+                        </div>
+                        <p>{company}</p>
                     </div>
+                    <div className="single-border"></div>
                 </div>
             </div>
         </main>
